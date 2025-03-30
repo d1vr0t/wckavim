4b867e56442f27a522f2958255f602bf28610e28
Alexander Weck <accounts@awck.de>
2025-03-30 18:08:58 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Standalone wkca neovim";
+
+  inputs = {
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixvim,
+    }:
+
+    let
+      vimconfig = import ./wckavim.nix;
+      pkgs = import nixpkgs { system =  "x86_64-linux"; };
+
+    in
+    {
+
+      packages.x86_64-linux.wckavim = nixvim.legacyPackages."x86_64-linux".makeNixvimWithModule {
+        inherit pkgs;
+        module = vimconfig;
+      };
+
+    };
+}
---
+++ wckavim.nix
@@ -0,0 +1,176 @@
+{ pkgs, lib, ... }:
+{
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+  opts = {
+    number = true;
+    mouse = "a";
+    cursorline = true;
+  };
+  colorschemes.onedark.enable = true;
+  plugins = {
+    dashboard.enable = true;
+    dashboard.settings = {
+      change_to_vcs_root = true;
+      config = {
+        footer = [
+          "The only winning move is not to play"
+        ];
+        header = [
+          "▗▖ ▗▖ ▗▄▄▖▗▖ ▗▖ ▗▄▖     ▗▖  ▗▖▗▄▄▄▖▗▖  ▗▖"
+          "▐▌ ▐▌▐▌   ▐▌▗▞▘▐▌ ▐▌    ▐▌  ▐▌  █  ▐▛▚▞▜▌"
+          "▐▌ ▐▌▐▌   ▐▛▚▖ ▐▛▀▜▌    ▐▌  ▐▌  █  ▐▌  ▐▌"
+          "▐▙█▟▌▝▚▄▄▖▐▌ ▐▌▐▌ ▐▌     ▝▚▞▘ ▗▄█▄▖▐▌  ▐▌"
+        ];
+        mru = {
+          limit = 20;
+        };
+        project = {
+          enable = false;
+        };
+        shortcut = [
+          {
+            action = {
+              __raw = "function(path) vim.cmd('Telescope find_files') end";
+            };
+            desc = "Files";
+            group = "Label";
+            icon = " ";
+            icon_hl = "@variable";
+            key = "f";
+          }
+        ];
+        week_header = {
+          enable = false;
+        };
+      };
+      theme = "hyper";
+    };
+    # Grep
+    telescope = {
+      enable = true;
+      extensions = {
+        file-browser.enable = true;
+        fzf-native.enable = true;
+        live-grep-args.enable = true;
+        ui-select.enable = true;
+        undo.enable = true;
+      };
+      keymaps = {
+
+        "<Leader>fg" = {
+          action = "live_grep";
+          options.desc = "Find w grep";
+        };
+
+        "<Leader>ff" = {
+          action = "find_files";
+          options.desc = "Find files";
+        };
+
+      };
+    };
+    # Filebrowser
+    nvim-tree.enable = true;
+    # Language Higlighting etc.
+    treesitter = {
+      enable = true;
+      grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+
+      settings = {
+        highlight.enable = true;
+        # Install missing Parsers
+        auto_install = true;
+        # Mark Code Blocks
+        incremental_selection.enable = true;
+        # Indent Code
+        indent.enable = true;
+      };
+    };
+
+    # Show Head of current function on top
+    treesitter-context = {
+      enable = true;
+    };
+    # QoL for code, showing definitions, current scope, renaming, etc...
+    treesitter-refactor = {
+      enable = true;
+      highlightCurrentScope.enable = true;
+      highlightDefinitions.enable = true;
+      navigation.enable = true;
+      # Rename with "grr"
+      smartRename.enable = true;
+    };
+
+    # Floating definitions, etc.:
+    treesitter-textobjects = {
+      enable = true;
+      lspInterop.enable = true;
+      move.enable = true;
+
+    };
+
+    # Language Server:
+    lsp = {
+      enable = true;
+      inlayHints = true;
+      servers = {
+        html.enable = true;
+        nixd = {
+          enable = true;
+          settings =
+            let
+              flake = ''(builtins.getFlake "/etc/nixos/flake")""'';
+            in
+            {
+              nixpkgs = {
+                expr = "import ${flake}.inputs.nixpkgs { }";
+              };
+              formatting = {
+                command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+              };
+            };
+
+        };
+      };
+    };
+
+    lsp-lines.enable = true;
+    lsp-signature.enable = true;
+    lsp-status.enable = true;
+    lspkind.enable = true;
+
+    # Autocompletion
+    cmp.enable = true;
+
+  };
+
+  keymaps = [
+    {
+      key = "<Space>";
+      action = "<Nop>";
+      options.desc = "Disable space in normal mode";
+      mode = "n";
+    }
+
+    {
+      key = "<c-n>";
+      action = "<cmd>NvimTreeToggle<CR>";
+      options.desc = "Toggle File browser";
+    }
+    {
+      key = "<Leader>F";
+      action = "<cmd>lua vim.lsp.buf.format()<CR>";
+      options.desc = "Format Code";
+    }
+    {
+      key = "<Leader>id";
+      action = "<cmd>lua vim.api.nvim_put({io.popen('uuidgen'):read():sub(1, -2)}, 'c', true, true)<CR>";
+      options.desc = "Insert UUID";
+    }
+
+  ];
+
+}
